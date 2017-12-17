53bf7743f249fb2d739b98a3a647acfeb4846f60
PavelDymkov <dymkov86@gmail.com>
2017-12-17 18:52:40 +0300

    1.0.0

--- www/scripts/bootstrap.js
+++ www/scripts/bootstrap.js
@@ -1,23 +1,23 @@
 "browser test", function () {
-	try {
-		eval("``.length")
-	} catch (error) {
-		showErrorMessage('Browser not supported. Use latest version');
-	}
+    try {
+        eval("``.length")
+    } catch (error) {
+        showErrorMessage('Browser not supported. Use latest version');
+    }
 } ();
 
 if (typeof web3 == "undefined") {
-	showErrorMessage(`Install <a href="https://metamask.io/">MetaMask</a> extension for your browser.`);
+    showErrorMessage(`Install <a href="https://metamask.io/">MetaMask</a> extension for your browser.`);
 }
 
 if (!web3.isConnected()) {
-	showErrorMessage(`Can't connect to testrpc`);
+    showErrorMessage(`Can't connect to testrpc`);
 }
 
 
 
 function showErrorMessage(message) {
-	document.write(message);
+    document.write(message);
 
-	throw new Error("can't start application");
+    throw new Error("can't start application");
 }
