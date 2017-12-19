81a847c84fbe77ee4df52c619077e470d7813c95
Дымков Павел Алексеевич <p.dymkov@prototypes.ru>
2017-12-19 12:00:02 +0300

    1.0.0

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,30 @@
-var Migrations = artifacts.require("./Migrations.sol");
+const Migrations = artifacts.require("./Migrations.sol");
+const CoinBox = artifacts.require("./CoinBox.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+const writeJSON = require("../lib/write-json.js");
+
+
+module.exports = function(deployer, network, accounts) {
+    if (network == "test") {
+        deployer.deploy(Migrations);
+    }
+
+    if (network == "development") {
+        const owner = accounts[0];
+        const periodInDays = 30;
+
+        deployer.deploy(CoinBox, periodInDays, { from: owner }).then(() => {
+            let deployedInfo = {
+                name: CoinBox.contractName,
+                address: CoinBox.address,
+                owner, periodInDays
+            };
+
+            writeJSON("./deployed-info.json", deployedInfo);
+            console.log(`deployed-info.json created`);
+
+            writeJSON("./www/deployed-info.json", deployedInfo);
+            console.log(`www/deployed-info.json created`);
+        });
+    }
 };
