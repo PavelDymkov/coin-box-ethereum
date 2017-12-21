9bffcc4c5eb9e219a3619ecca294fceed58f3a04
Дымков Павел Алексеевич <p.dymkov@prototypes.ru>
2017-12-21 18:36:21 +0300

    1.0.0

--- local-etherium/run-network.js
+++ local-etherium/run-network.js
@@ -7,46 +7,39 @@ const genesisJSON = readJSON("local-etherium/genesis.json");
 const config = readJSON("local-etherium/config.json");
 
 
-exeCommand("rm", { "-rf": "temp" });
-exeCommand("mkdir", null, "temp");
+executeCommand(`rm -rf temp`);
+executeCommand(`mkdir temp`);
 
-exeCommand("geth", { "--datadir": "./temp" }, "init genesis.json");
-exeCommand("geth", { "--datadir": "./temp" }, "js prepare-network.js");
+executeCommand(`geth --datadir ./temp init genesis.json`);
+executeCommand(`geth --datadir ./temp js prepare-network.js`);
 
 console.log("geth has been ran");
 
-exeCommand("geth", {
-    "--datadir": "./temp",
+executeCommand(`geth
+    --datadir ./temp
 
-    "--identity": "LocalTestNetwork",
-    "--networkid": genesisJSON.config.chainId,
-    "--nodiscover": null,
-    "--maxpeers": "0",
-    "--ipcdisable": null,
-    "--mine": null,
+    --networkid ${genesisJSON.config.chainId}
+    --nodiscover
+    --maxpeers 0
+    --ipcdisable
+    --mine
 
-    "--rpc": null,
-    "--rpcport": String(config.rpcport),
-    "--rpccorsdomain": '"*"',
-    "--rpcapi": '"admin,db,eth,debug,miner,net,shh,txpool,personal,web3"',
+    --rpc
+    --rpcport ${config.rpcport}
+    --rpccorsdomain "*"
+    --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3"
 
-    "--ws": null,
-    "--wsport": String(config.wsport),
-    "--wsorigins": '"*"',
-    "--wsapi": '"admin,db,eth,debug,miner,net,shh,txpool,personal,web3"'
-});
+    --ws
+    --wsport ${config.wsport}
+    --wsorigins "*"
+    --wsapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3"
+`);
 
 
-function exeCommand(command, options, args) {
-    args = typeof args == "string" ? args.split(/\s+/) : [];
+function executeCommand(command) {
+    let args = command.split(/\s+/).filter(source => source);
 
-    for (let key in options) if (options.hasOwnProperty(key)) {
-        if (options[key]) args.unshift(options[key]);
-
-        args.unshift(key);
-    }
-
-    // console.log("execute:\n", `${command} ${args.join(" ")}`, "\n");
+    command = args.shift();
 
     let result = spawnSync(command, args, {
         cwd: __dirname
