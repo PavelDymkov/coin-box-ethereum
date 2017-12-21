a4ca04171888c5418d312e522e856a0aaca5e98d
Дымков Павел Алексеевич <p.dymkov@prototypes.ru>
2017-12-21 18:05:45 +0300

    1.0.0

--- local-etherium/connect-to-network.js
+++ local-etherium/connect-to-network.js
@@ -1,12 +1,23 @@
-const Web3 = require("web");
-const readJSON = require("lib/read-json.js");
+const Web3 = require("web3");
+const readJSON = require("../lib/read-json.js");
 
 
 const CoinBox = readJSON("build/contracts/CoinBox.json");
 const config = readJSON("local-etherium/config.json");
 const deployInfo = readJSON("local-etherium/deployed-info.json");
 const web3 = new Web3(`http://localhost:${config.rpcport}`);
+// const web3 = new Web3(`ws://localhost:${config.wsport}`); // not work
 
 (async function () {
-    // const constract = new web3.eth.Contract(CoinBox.abi, CoinBox.address);
+    const constract = new web3.eth.Contract(CoinBox.abi, deployInfo.address);
+
+    constract.events.allEvents(function (error) {
+        if (error) return console.log(error.message);
+    });
+
+    await constract.methods.putCoins().send({ from: deployInfo.owner, value: 10 });
+
+    let balance = await constract.methods.getBalance().call({ from: deployInfo.owner });
+
+    console.log("balance", balance);
 } ());
